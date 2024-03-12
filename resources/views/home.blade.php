f6b47fe9ec17205b255acfb27a454ee97da8ead7
Axhu99 <xhuliodreni@icloud.com>
2024-03-12 19:32:59 +0100

    First layout

---
+++ resources/views/home.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.main')
+
+@section('title', 'Home')
+
+@section('main-content')
+    <h1>Benvenuti alla nostro sito sui treni</h1>
+@endsection
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -1,3 +1,23 @@
-<div>
-    <!-- People find pleasure in different ways. I find it in keeping my mind clear. - Marcus Aurelius -->
-</div>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>{{ env('APP_NANE') }} @yield('title')</title>
+
+    @vite('resources/js/app.js')
+</head>
+
+<body>
+    {{-- HEADER --}}
+    @include('includes.header')
+    {{-- MAIN --}}
+    <main class="container my-4">
+        @yield('main-content')
+    </main>
+
+    {{-- FOOTER --}}
+</body>
+
+</html>
